ccc9e390a87ee1222ec06f550dddea1e27d905e4
SierikovRoman <rebit1love@gmail.com>
2018-02-07 21:07:33 +0200

    Wooder template

---
+++ README.md
@@ -0,0 +1,16 @@
+# Wooder template
+
+Simple realized template of "Wooder" project
+
+## Built With
+
+* [Bootstap 4](https://getbootstrap.com) - Web framework
+
+## Authors
+
+* **Viacheslav Olianishyn** - *V.O.* - [Behance](https://www.behance.net/Olianishyn) - UI/UX, Graphic Designer
+* **Roman Sierikov** - *R.S.* - [GitHub](https://github.com/SierikovRoman) - Frontend developer
+
+## Live
+
+https://sierikovroman.github.io/wooder/
---
+++ js/index.js
@@ -0,0 +1,32 @@
+
+/* MENU */
+function showMenu() {
+    var w = $(window).width();
+    $(".menu_slider").css('width', w + "px");
+    $(".menu_link").css('opacity', 0.5);
+}
+
+function closeMenu() {
+    var w = $(window).width();
+    $(".menu_slider").css('width', '0' + "px");
+    $(".menu_link").css('opacity', 0);
+}
+
+/* INFO */
+function showInfo() {
+    var h = $(window).height();
+    $(".info_wrapper").css('height', h + "px");
+    $(".information_block").css({'display': 'block', 'opacity': 1});
+}
+
+function closeInfo() {
+    var h = $(window).height();
+    $(".info_wrapper").css('height', '0' + "px");
+    $(".information_block").css('opacity', 0).fadeOut(1000, linear);
+}
+
+/* smooth scroll */
+var scroll = new SmoothScroll('a[data-scroll]', {
+    speed: 2500,
+    easing: 'easeInOutCubic'
+});

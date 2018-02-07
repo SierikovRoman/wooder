e0d5e46f3da4edbd0922f7f6e5d5348b6329efb0
SierikovRoman <rebit1love@gmail.com>
2018-02-08 01:15:48 +0200

    fixed one more time - hope it's last ;)

--- js/index.js
+++ js/index.js
@@ -3,15 +3,13 @@
 function showMenu() {
     var w = $(window).width();
     $(".menu_slider").css('width', w + "px");
-    $(".menu_link").css('opacity', 0.5);
-    $(".menu_slider_list").css('display', 'block');
+    $(".menu_link").css('opacity', 0.5);    
 }
 
 function closeMenu() {
     var w = $(window).width();
     $(".menu_slider").css('width', '0' + "px");
     $(".menu_link").css('opacity', 0).fadeOut(1000, linear);
-    $(".menu_slider_list").css('display', 'none');
 }
 
 /* INFO */
@@ -26,7 +24,7 @@ function closeInfo() {
     var h = $(window).height();
     $(".info_wrapper").css('height', '0' + "px");
     $(".information_block").css('opacity', 0).fadeOut(1000, linear);
-    $(".information_block").css('display', 'none');
+    $(".information_block").css('display', none);
 }
 
 /* smooth scroll */
